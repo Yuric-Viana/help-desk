e9e1b518cfb7a05cae338e2ffb994e20a5bc97f8
Yuri Viana <ycviana@sga.pucminas.br>
2026-01-27 15:18:29 -0300

    feat: implementar constante utilitária para o menu

---
+++ src/lib/utils/menuListByRole.ts
@@ -0,0 +1,50 @@
+export const menuListByRole = {
+  admin: [
+    {
+      label: "Chamados",
+      icon: "/icons/list-default.svg",
+      iconHover: "/icons/list-hover.svg",
+      path: "/tickets",
+    },
+    {
+      label: "Técnicos",
+      icon: "/icons/users-default.svg",
+      iconHover: "/icons/users-hover.svg",
+      path: "/users/technicians",
+    },
+    {
+      label: "Clientes",
+      icon: "/icons/client-default.svg",
+      iconHover: "/icons/client-hover.svg",
+      path: "/users/clients",
+    },
+    {
+      label: "Serviços",
+      icon: "/icons/wrench-default.svg",
+      iconHover: "/icons/wrench-hover.svg",
+      path: "/services",
+    },
+  ],
+  client: [
+    {
+      label: "Meus chamados",
+      icon: "/icons/list-default.svg",
+      iconHover: "/icons/list-hover.svg",
+      path: "/tickets",
+    },
+    {
+      label: "Criar chamado",
+      path: "/",
+      icon: "/icons/plus-default.svg",
+      iconHover: "/icons/plus-hover.svg",
+    },
+  ],
+  technical: [
+    {
+      label: "Meus chamados",
+      icon: "/icons/list-default.svg",
+      iconHover: "/icons/plus-hover.svg",
+      path: "/tickets",
+    },
+  ],
+};

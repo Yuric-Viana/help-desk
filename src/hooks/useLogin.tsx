404de7b5865ea1ce6f8e97976025e84a832eb027
Yuri Viana <ycviana@sga.pucminas.br>
2026-01-31 14:34:14 -0300

    feat: implementar hook de de login

---
+++ src/hooks/useLogin.tsx
@@ -0,0 +1,32 @@
+import { SignInFormData } from "@/schemas/login";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+export function useLogin() {
+  const router = useRouter();
+
+  async function loginAction(values: SignInFormData) {
+    try {
+      const response = await signIn("credentials", {
+        redirect: false,
+        email: values.email,
+        password: values.password,
+      });
+
+      if (response?.error) {
+        toast.error("E-mail e/ou senha inválidos.");
+        return;
+      }
+
+      router.replace("/tickets");
+      router.refresh();
+    } catch (error) {
+      toast.error("Erro ao tentar fazer login. Tente novamente mais tarde.");
+    }
+  }
+
+  return {
+    loginAction,
+  };
+}

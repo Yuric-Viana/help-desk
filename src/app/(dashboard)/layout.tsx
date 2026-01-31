31953d8f10eb93c0b88b554bf77effad8992c2f6
Yuri Viana <ycviana@sga.pucminas.br>
2026-01-31 16:18:52 -0300

    feat: implementar sidebar do desktop ao layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,11 +1,15 @@
 import { MobileHeader } from "@/components/MobileHeader";
+import { Sidebar } from "@/components/Sidebar";
 import { ReactNode } from "react";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="bg-app-gray-100 min-h-screen w-full">
+    <div className="bg-app-gray-100 min-h-screen w-full lg:grid lg:grid-cols-[200px_1fr]">
+      <div className="hidden lg:block">
+        <Sidebar />
+      </div>
       <MobileHeader />
-      {children}
+      <main className="bg-app-gray-600 mt-3 rounded-t-2xl">{children}</main>
     </div>
   );
 }

d5849658383501696ffc593e39efa212160bd539
Yuri Viana <ycviana@sga.pucminas.br>
2026-02-04 22:35:11 -0300

    chore(types): adiciona UserRole às definições de tipo do NextAuth

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,9 +1,22 @@
 import { DefaultSession } from "next-auth";
+import { UserRole } from "./generated/prisma/enums";
 
 declare module "next-auth" {
+  interface User {
+    role: UserRole;
+  }
+
   interface Session {
     user: {
       id: string;
+      role: UserRole;
     } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role: UserRole;
+    id: string;
   }
 }

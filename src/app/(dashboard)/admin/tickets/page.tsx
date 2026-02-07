2ff4525864a3e942289aee7c27f2a5e3ad41e680
Yuri Viana <ycviana@sga.pucminas.br>
2026-02-07 00:17:11 -0300

    feat: implementar tabela de tickets

---
+++ src/app/(dashboard)/admin/tickets/page.tsx
@@ -0,0 +1,58 @@
+import { prisma } from "@/lib/prisma";
+import { columns, TicketRow } from "./columns";
+import { DataTable } from "./data-table";
+
+export default async function TicketsPage() {
+  const tickets = await prisma.ticket.findMany({
+    include: {
+      technician: true,
+      client: true,
+      ticketServices: {
+        include: {
+          service: true,
+        },
+      },
+    },
+  });
+
+  const formattedTickets: TicketRow[] = tickets.map((ticket) => {
+    const totalAmount = ticket.ticketServices.reduce((acc, item) => {
+      return acc + (item.priceSnapshot || 0);
+    }, 0);
+
+    const servicesNames = ticket.ticketServices
+      .map((item) => item.service.title)
+      .join(", ");
+
+    return {
+      ...ticket,
+      updatedAt: ticket.updatedAt?.toISOString(),
+      openedAt: ticket.openedAt.toISOString(),
+      closedAt: ticket.closedAt?.toISOString() || null,
+
+      amount: totalAmount,
+      serviceName: servicesNames,
+
+      client: {
+        ...ticket.client,
+        createdAt: ticket.client.createdAt.toISOString(),
+        updatedAt: ticket.client.updatedAt?.toISOString() || null,
+        emailVerified: ticket.client.emailVerified?.toISOString() || null,
+      },
+      technician: {
+        ...ticket.technician,
+        createdAt: ticket.technician.createdAt.toISOString(),
+        updatedAt: ticket.technician.updatedAt?.toISOString() || null,
+        emailVerified: ticket.technician.emailVerified?.toISOString() || null,
+      },
+    };
+  });
+
+  return (
+    <div className="container mt-14 w-full space-y-6 px-6 md:px-12">
+      <h1 className="text-brand-dark text-xl font-bold">Chamados</h1>
+
+      <DataTable columns={columns} data={formattedTickets} />
+    </div>
+  );
+}

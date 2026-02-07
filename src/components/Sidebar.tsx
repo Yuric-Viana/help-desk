4994acfc98d5bd29dd662b564ec454e7c4fdf131
Yuri Viana <ycviana@sga.pucminas.br>
2026-02-07 00:21:09 -0300

    feat: buscar pelo nome do usuário logado

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,14 @@
+import { getServerSession } from "next-auth";
 import { AppFooter } from "./AppFooter";
 import { AppHeader } from "./AppHeader";
 import { NavLinks } from "./NavLinks";
+import { authOptions } from "@/lib/auth";
 
-export function Sidebar() {
+export async function Sidebar() {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user.name) return;
+
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       <div className="border-app-gray-200 mt-3 mb-auto border-b px-5 py-6">
@@ -14,7 +20,7 @@ export function Sidebar() {
       </div>
 
       <div>
-        <AppFooter />
+        <AppFooter user={{ name: session.user.name }} />
       </div>
     </div>
   );

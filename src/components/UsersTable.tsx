a54a829218c6bb9838084065b99212e3597cf0a9
Yuri Viana <ycviana@sga.pucminas.br>
2026-02-11 12:14:05 -0300

    feat: refatorar colunas para a tabela de usuários para add hook

---
+++ src/components/UsersTable.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { DataTable } from "@/components/data-table";
+import { InitialsAvatar } from "@/components/InitialsAvatar";
+import { TimeSlotsTechnicial } from "@/components/TimeSlotsTechnicial";
+import { Button } from "@/components/ui/button";
+import { useIsRoute } from "@/hooks/useIsRoute";
+import { cn } from "@/lib/classMerge";
+import { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+export type Users = {
+  id: string;
+  name: string;
+  email: string;
+  availabilities: {
+    schedules: string[];
+  }[];
+};
+
+type UsersTableProps = {
+  data: Users[];
+};
+
+const AvailabilitiesCell = () => {
+  const pathname = usePathname();
+
+  const rotesToHide = ["/admin/users/client"];
+
+  const isClient = rotesToHide.includes(pathname);
+
+  return isClient;
+};
+
+export function UsersTable({ data }: UsersTableProps) {
+  const isClient = useIsRoute({ user: "client" });
+
+  const columns: ColumnDef<Users>[] = [
+    {
+      accessorKey: "name",
+      header: () => <p className="text-app-gray-400 text-sm">Nome</p>,
+      cell: ({ row }) => {
+        const user = row.original.name;
+
+        return (
+          <div className="flex items-center gap-3">
+            <div className="bg-brand-dark flex h-5 w-5 items-center justify-center rounded-full">
+              {<InitialsAvatar className="text-[8.75px]" name={user} />}{" "}
+            </div>
+            <p>{user}</p>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "email",
+      header: () => {
+        const isClient = AvailabilitiesCell();
+
+        return (
+          <p className={cn(["text-app-gray-400 hidden text-sm md:table-cell"])}>
+            E-mail
+          </p>
+        );
+      },
+      cell: ({ row }) => {
+        const email = row.original.email;
+
+        return <div className="hidden md:table-cell">{email}</div>;
+      },
+    },
+    {
+      accessorKey: "availabilities",
+      header: () => {
+        return !isClient ? (
+          <p className="text-app-gray-400 text-sm">Disponibilidade</p>
+        ) : null;
+      },
+      cell: ({ row }) => {
+        const schedules = row.original.availabilities[0];
+
+        return schedules && <TimeSlotsTechnicial availabilities={schedules} />;
+      },
+    },
+    {
+      accessorKey: "id",
+      header: "",
+      cell: ({ row }) => {
+        return (
+          <div className="flex justify-end gap-2">
+            <Button size="icon-sm" variant="ghost" className="bg-app-gray-500">
+              <Image
+                src="/icons/trash.svg"
+                alt="Ícone de lixeira"
+                width={14}
+                height={14}
+              />
+            </Button>
+            <Button size="icon-sm" variant="ghost" className="bg-app-gray-500">
+              <Image
+                src="/icons/pen-line.svg"
+                alt="Ícone de lápis"
+                width={14}
+                height={14}
+              />
+            </Button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  return <DataTable columns={columns} data={data} />;
+}

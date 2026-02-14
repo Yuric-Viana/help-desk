87f37768650b5eb13179a5c582bb7a9158f75023
Yuri Viana <ycviana@sga.pucminas.br>
2026-02-14 19:25:39 -0300

    feat: implementar server action para atualização do usuário

---
+++ src/actions/UpdateClient.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { UpdateClientFormData } from "@/schemas/update-client";
+import { revalidatePath } from "next/cache";
+
+interface UpdateClientProps {
+  userId: string;
+  data: UpdateClientFormData;
+}
+
+export const UpdateClient = async ({ userId, data }: UpdateClientProps) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!user) {
+    throw new Error("Usuário não encontrado");
+  }
+
+  await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    select: {
+      name: true,
+    },
+    data: {
+      email: data.email,
+    },
+  });
+
+  revalidatePath("/admin/users/clients");
+};

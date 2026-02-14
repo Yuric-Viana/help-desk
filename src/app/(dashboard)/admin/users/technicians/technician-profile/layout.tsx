59663471e58b547ff7f069110f971432628e1304
Yuri Viana <ycviana@sga.pucminas.br>
2026-02-13 21:57:37 -0300

    refactor: refatorar layout para página de perfil do técnico

--- src/app/(dashboard)/admin/users/technicians/technician-profile/layout.tsx
+++ src/app/(dashboard)/admin/users/technicians/technician-profile/layout.tsx
@@ -2,35 +2,47 @@
 
 import { BackBottom } from "@/components/BackBottom";
 import { Button } from "@/components/ui/button";
+import { useTechnicianProfile } from "@/hooks/useTechnicianProfile";
 import { ReactNode } from "react";
-import { FormProvider, useForm } from "react-hook-form";
+import { FormProvider } from "react-hook-form";
 
 export default function TechnicianProfileLayout({
   children,
 }: {
   children: ReactNode;
 }) {
-  const methods = useForm();
+  const { handleSave, methods } = useTechnicianProfile();
 
   return (
-    <div className="space-y-4 px-6 py-7 lg:px-45.75 lg:py-13">
-      <div className="lg:flex lg:items-center lg:justify-between">
+    <FormProvider {...methods}>
+      <form
+        onSubmit={methods.handleSubmit(handleSave)}
+        className="space-y-4 px-6 py-7 lg:px-45.75 lg:py-13"
+      >
         <div>
-          <BackBottom className="has-[>svg]:px-0" />
-          <h1 className="text-brand-dark text-xl font-bold">
-            Perfil de técncico
-          </h1>
+          <div className="lg:flex lg:items-center lg:justify-between">
+            <div>
+              <BackBottom className="has-[>svg]:px-0" />
+              <h1 className="text-brand-dark text-xl font-bold">
+                Perfil de técncico
+              </h1>
+            </div>
+            <div className="mt-3 flex gap-2">
+              <Button variant="outline" type="button" className="flex-1">
+                Cancelar
+              </Button>
+              <Button
+                type="submit"
+                className="flex-1"
+                disabled={methods.formState.isSubmitting}
+              >
+                {methods.formState.isSubmitting ? "Salvando..." : "Salvar"}
+              </Button>
+            </div>
+          </div>
         </div>
-        <div className="mt-3 flex gap-2">
-          <Button variant="outline" type="button" className="flex-1">
-            Cancelar
-          </Button>
-          <Button type="submit" className="flex-1">
-            Salvar
-          </Button>
-        </div>
-      </div>
-      <FormProvider {...methods}>{children}</FormProvider>
-    </div>
+        {children}
+      </form>
+    </FormProvider>
   );
 }

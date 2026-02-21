c1f1989b25d08e74793ed0afbc39936e1386e52d
Yuri Viana <ycviana@sga.pucminas.br>
2026-02-21 20:04:16 -0300

    feat: adicionar props opcional e filtragem na busca pelos tickets

--- src/actions/GetAllTickets.ts
+++ src/actions/GetAllTickets.ts
@@ -1,9 +1,19 @@
 "use server";
 
 import { prisma } from "@/lib/prisma";
+import { TicketStatus } from "../../generated/prisma/enums";
 
-export const GetAllTickets = async () => {
+interface GetAllTicketsProps {
+  status?: TicketStatus;
+  technicianId?: string;
+}
+
+export const GetAllTickets = async (props?: GetAllTicketsProps) => {
   return await prisma.ticket.findMany({
+    where: {
+      status: props?.status,
+      technicianId: props?.technicianId,
+    },
     include: {
       technician: true,
       client: true,
--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,6 +1,6 @@
 export const routePermissions = {
   "/admin": ["admin"],
-  "/technician": ["technician"],
+  "/technician": ["technical"],
   "/portal": ["client"],
 };
 

23967dff90662b6e8bb52a77f2d13908bbf7e2f7
Yuri Viana <ycviana@sga.pucminas.br>
2026-02-21 20:15:17 -0300

    refactor: refatorar componente TicketDetailsAside

--- src/components/TicketDetailsAside.tsx
+++ src/components/TicketDetailsAside.tsx
@@ -1,5 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import { InitialsAvatar } from "./InitialsAvatar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { cn } from "@/lib/classMerge";
 
 interface TicketDetailsAsideParams {
   ticketId: number;
@@ -8,6 +11,8 @@ interface TicketDetailsAsideParams {
 export async function TicketDetailsAside({
   ticketId,
 }: TicketDetailsAsideParams) {
+  const session = await getServerSession(authOptions);
+
   const ticket = await prisma.ticket.findFirst({
     where: {
       id: ticketId,
@@ -23,6 +28,7 @@ export async function TicketDetailsAside({
   });
 
   if (!ticket) return;
+  if (!session) return;
 
   return (
     <div className="border-app-gray-500 rounded-lg border p-5 lg:col-span-2 lg:col-start-4 lg:grid lg:min-h-77 lg:self-start">
@@ -42,7 +48,12 @@ export async function TicketDetailsAside({
       </div>
 
       <small className="text-app-gray-400 text-xs font-bold">Valores</small>
-      <div className="mb-4 flex items-center justify-between">
+      <div
+        className={cn([
+          "mb-4 flex items-center justify-between",
+          session.user.role === "technical" && "mb-1",
+        ])}
+      >
         <p className="text-app-gray-200 text-xs">Preço base</p>
         <p className="text-app-gray-200 text-xs">
           {Intl.NumberFormat("pt-BR", {
@@ -52,7 +63,7 @@ export async function TicketDetailsAside({
         </p>
       </div>
 
-      {ticket.ticketServices.length > 1 && (
+      {ticket.ticketServices.length > 1 && session.user.role === "admin" && (
         <>
           <small className="text-app-gray-400 text-xs font-bold">
             Adicionais
@@ -80,6 +91,27 @@ export async function TicketDetailsAside({
           </div>
         </>
       )}
+
+      {ticket.ticketServices.length > 1 &&
+        session.user.role === "technical" && (
+          <div className="flex items-center justify-between">
+            <p className="text-app-gray-200 text-xs">Adicionais</p>
+            <p className="text-app-gray-200 text-xs">
+              {Intl.NumberFormat("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              }).format(
+                Number(
+                  ticket.ticketServices.reduce((acc, item, index) => {
+                    if (index === 0) return acc;
+
+                    return acc + item.priceSnapshot;
+                  }, 0),
+                ),
+              )}
+            </p>
+          </div>
+        )}
 
       <div className="border-app-gray-500 mt-4 flex items-center justify-between border-t">
         <h4 className="text-app-gray-200 pt-4 text-sm font-bold">Total</h4>

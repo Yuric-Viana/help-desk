1a076a1886e754376df530ad601ba35ccbfd1edd
Yuri Viana <ycviana@sga.pucminas.br>
2026-02-21 20:13:48 -0300

    refactor: refatorar componente de sidebar

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -6,24 +6,30 @@ import { PlusIcon } from "lucide-react";
 import { useIsRoute } from "@/hooks/useIsRoute";
 import Link from "next/link";
 import { PopupService } from "./PopupService";
+import { cn } from "@/lib/classMerge";
 
 interface PageContainerProps {
   children: ReactNode;
   title: string;
   id?: string;
   actionRef?: string;
+  className?: string;
 }
 
 export function PageContainer({
   title,
   children,
   actionRef,
+  className,
 }: PageContainerProps) {
   const isRouteTechnicians = useIsRoute({ ref: "/admin/users/technicians" });
   const isRouteServices = useIsRoute({ ref: "/admin/services" });
+  const isRouteTickets = useIsRoute({ ref: "/technician/tickets" });
 
   return (
-    <div className="container mt-7 w-full space-y-6 md:px-12">
+    <div
+      className={cn(["container mt-7 w-full space-y-6 md:px-12", className])}
+    >
       <div className="flex items-center justify-between">
         <h1 className="text-brand-dark text-xl font-bold">{title}</h1>
         {isRouteTechnicians && actionRef && (
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import { useState } from "react";
 import { AppFooter } from "./AppFooter";
 import { AppHeader } from "./AppHeader";
 import { NavLinks } from "./NavLinks";
+import { Button } from "./ui/button";
+import { ProfileOptions } from "./ProfileOptions";
 
-export async function Sidebar() {
+export function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       <div className="border-app-gray-200 mt-3 mb-auto border-b px-5 py-6">
@@ -13,8 +20,19 @@ export async function Sidebar() {
         <NavLinks />
       </div>
 
-      <div>
-        <AppFooter />
+      <div className="relative">
+        <Button
+          variant="ghost"
+          asChild
+          onClick={() => (isOpen === true ? setIsOpen(false) : setIsOpen(true))}
+        >
+          <AppFooter />
+        </Button>
+        {isOpen && (
+          <div className="absolute bottom-2 left-full w-50">
+            <ProfileOptions />
+          </div>
+        )}
       </div>
     </div>
   );
